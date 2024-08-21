9b9c9874a63e5629f0f2be6ea9627f71f93672de
Thamarai-selvan-G <thamaraiselvan878@gmail.com>
2024-08-21 14:30:59 +0530

    swiper fixing

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from "react";
-import "./Home.css"
+import "./Home.css";
+import axios from "axios";
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
 import HomeCover1 from "../Components/HomeCover/HomeCover1";
-import TamSwiper from "../Components/TamSwiper";
-import axios from "axios";
-// import  { useRef } from 'react';
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
 
-import '../Components/TamCardSwiper/TamswiperComp.css';
+import "../Components/TamCardSwiper/TamswiperComp.css";
 
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { Navigation } from "swiper/modules";
 
 const Home = () => {
   const [apiData, setApiData] = useState([]);
@@ -26,7 +24,7 @@ const Home = () => {
       let mydata = await axios.get("http://localhost:4000/movie/viewall");
       let datas = mydata.data;
       // console.log(datas);
-      return setApiData(datas.message)
+      return setApiData(datas.message);
     };
     apiCall();
   }, []);
@@ -35,33 +33,24 @@ const Home = () => {
 
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       {/* <HomeCover1 /> */}
-      
-      <>
-      <Swiper
-        slidesPerView={4}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide><TamSwiper  tamMovieData={apiData}/></SwiperSlide>
-        {/* <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide> */}
-      </Swiper>
-    </>
-      
+      {apiData.map((val, index) => {
+        return (
+          <div key={index}>
+            <Swiper
+              navigation={true}
+              modules={[Navigation]}
+              className="mySwiper">
+              <SwiperSlide>
+                <img src={val.thumpNile} alt={val.movieName} />
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        );
+      })}
 
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
