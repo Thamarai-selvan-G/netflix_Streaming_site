0a531ff92c1eb08ad4cf9b2e33d731cf684a287e
Thamarai-selvan-G <thamaraiselvan878@gmail.com>
2024-08-26 15:30:20 +0530

    correction works

--- src/Components/TamSwiper.jsx
+++ src/Components/TamSwiper.jsx
@@ -1,47 +1,93 @@
-import React, { useState } from 'react'
-import './TamSwiper.css'
-import movieVideo from "../Assets/SampleVideos/sample.mp4"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./TamSwiper.css";
+import movieVideo from "../Assets/SampleVideos/sample.mp4";
 import { ImCancelCircle } from "react-icons/im";
-const TamSwiper = (props) => {
-     
-    let  = props.tamMovieData
-    console.log(props)
-    // console.log(movieApiData.map(val=>console.log(val)));
-    
-    
 
-    const [display,setDisplay] = useState(false)
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+
+const TamSwiper = () => {
+  const [apiData, setApiData] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [display, setDisplay] = useState(false);
+
+  useEffect(() => {
+    const apiCall = async () => {
+      let mydata = await axios.get(
+        "https://netflixbackend-dysz.onrender.com/movie/viewall"
+      );
+      let datas = mydata.data;
+      setApiData(datas.message);
+    };
+    apiCall();
+  }, []);
+
+  const displayData = (id) => {
+    let filterData = apiData.find((val) => id === val.id);
+    setSelectedMovie(filterData);
+    setDisplay(true);
+  };
 
   return (
     <>
-        {
-            movieApiData.map((value,id)=>{
-               return( <div key={id} className='parentConrainer'>
-                <img src={value.thumpNile} alt=""  className='thumpNile' onClick={()=>setDisplay(!display)}/>
-                   { display === true ?  
-                   <div className="movieContent" >
-                        <div className="movieVideo">
-                            <video controls autoPlay muted loop src={movieVideo} className='movie'></video>
-                        </div>
-                        <div className="aboutMovie">
-                            <div className='basicinfo'>
-                                <h2 className='movieName'> {value.movieName} </h2>
-                                <h4 className='movieYear'> {value.relesedYear} </h4>
-                                <h4 className='movieRating'> {value.rating} </h4>
-                                <h3 className='movieCensor'> {value.censor} </h3>
-                            </div>
-                            <div className="movieDes">
-                                <h4> Genre : {value.genre}</h4>
-                                <p> {value.about} </p>
-                            </div>
-                        </div>
-                        <p className='close' onClick={()=>setDisplay(false)}> <ImCancelCircle /></p>
-                    </div> : null}
-             </div>)
-            })
-        }
+      <div className="swiper-container">
+        <Swiper
+          slidesPerView={6}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+          spaceBetween={5}
+          initialSlide={0}>
+          {apiData.map((val) => (
+            <SwiperSlide key={val.id}>
+              <img
+                className="parentConrainer"
+                src={val.thumpnile}
+                alt={val.movieName}
+                onClick={() => displayData(val.id)}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        {selectedMovie && display && (
+          <div className="movieContent">
+            <div className="movieVideo">
+              <video
+                controls
+                autoPlay
+                muted
+                loop
+                src={movieVideo}
+                className="movie"></video>
+            </div>
+            <div className="aboutMovie">
+              <div className="basicinfo">
+                <h2 className="movieName">{selectedMovie.movieName}</h2>
+                <h4 className="movieYear">{selectedMovie.relesedYear}</h4>
+                <h4 className="movieRating">{selectedMovie.rating}</h4>
+                <h3 className="movieCensor">{selectedMovie.censor}</h3>
+              </div>
+              <div className="movieDes">
+                <h4>Genre: {selectedMovie.genre}</h4>
+                <p>{selectedMovie.about}</p>
+              </div>
+            </div>
+            <p className="close" onClick={() => setDisplay(false)}>
+              <ImCancelCircle />
+            </p>
+          </div>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default TamSwiper
+export default TamSwiper;
--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,75 +1,18 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import "./Home.css";
-import axios from "axios";
+
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
 import HomeCover1 from "../Components/HomeCover/HomeCover1";
+import TamSwiper from  "../Components/TamSwiper"
 
-import { useRef } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-import '../Components/TamCardSwiper/TamswiperComp.css';
-
-// import required modules
-import { Pagination, Navigation } from 'swiper/modules';
 const Home = () => {
-  const [apiData, setApiData] = useState([]);
-  const [display, setDisplay] = useState(false);
-  const [selectedMovie, setSelectedMovie] = useState(null);
 
-  useEffect(() => {
-    const apiCall = async () => {
-      let mydata = await axios.get("https://netflixbackend-dysz.onrender.com/movie/viewall");
-      let datas = mydata.data;
-      // console.log(datas);
-      return setApiData(datas.message);
-    };
-    apiCall();
-  }, []);
-
-  console.log(apiData);
-
-  const handleSlideClick = (movie) => {
-    setSelectedMovie(movie);
-    setDisplay(true);
-  };
   return (
     <div>
       <Navbar />
-      {/* <HomeCover1 /> */}
-      <Swiper
-        onSwiper={setSwiperRef}
-        slidesPerView={3}
-        centeredSlides={true}
-        spaceBetween={5}
-        pagination={{
-          type: 'false',
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-      >
-       {
-          apiData.map((movie) => (
-            <SwiperSlide key={movie.id}>
-              <img 
-                id={movie.id} 
-                src={movie.thumpNile} 
-                alt={movie.movieName} 
-                onClick={() => handleSlideClick(movie)} 
-              />
-            </SwiperSlide>
-          ))
-        }
-      </Swiper>
-      { display && <TamSwiper   tamMovieData={selectedMovie} onClose={() => setDisplay(false)} />}
-
+      <HomeCover1 />
+      <TamSwiper/>
       {/* <Footer /> */}
     </div>
   );
